e1354536fa40177e13a5a54187ff49f4ced119f0
Oliver Tacke <o.tacke@posteo.de>
2020-06-04 13:23:54 +0200

    Add initial release

---
+++ src/entries/h5peditor-boilerplate.js
@@ -0,0 +1,6 @@
+import "../styles/h5peditor-boilerplate.css";
+import Boilerplate from "../scripts/h5peditor-boilerplate";
+
+// Load library
+H5PEditor = H5PEditor || {};
+H5PEditor.widgets.boilerplate = Boilerplate;
